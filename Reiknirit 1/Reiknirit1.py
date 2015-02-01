6e76a4ff323026735449a1af043900f769971b67
Arnar <addi1204@gmail.com>
2015-02-01 13:11:19 +0000

    stuff

--- Reiknirit 1/Reiknirit1.py
+++ Reiknirit 1/Reiknirit1.py
@@ -3,44 +3,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def myStemplot(x,y,xl,yl,name):
+    plt.figure(name)
+    markerline, stemlines, baseline = plt.stem(x,y,linefmt = '-.')
+    plt.setp(stemlines, 'linewidth','2.0')
+    plt.setp(baseline, 'color','r', 'linewidth', 2)
+    plt.axis([x[0],x[len(x)-1],0,2])
+    plt.ylabel(yl)
+    plt.xlabel(xl)
+    plt.grid(True)
+    plt.show()
 
-y = np.zeros((1,15))
+y = np.zeros(15)
 n = np.linspace(-8,6,15)
 
-y[0,11] = 1
+y[11] = 1
 
 print(len(n))
-print(len(y[0]))
+print(len(y))
 print(n)
 print(y)
-markerline, stemlines, basline = plt.stem(n,y[0],linefmt = 'b')
-plt.setp(stemlines, 'linewidth','2.0')
-plt.axis([n[0],n[len(n)-1],0,2])
-plt.ylabel('')
-plt.xlabel('n')
-plt.show()
+myStemplot(n,y,'n','y[n]','Reiknirit1')
 
 print(n)
 print(y)
 
 
-x = np.zeros((1,15))
-x[0,9] = 1
-x[0,10] = 1
-x[0,11] = 1
-x[0,12] = 1
+x = np.zeros(15)
+x[9] = 1
+x[10] = 1
+x[11] = 1
+x[12] = 1
 
-markerline, stemlines1, basline = plt.stem(n,x[0],linefmt = 'b')
-plt.setp(stemlines1, 'linewidth','2.0')
-plt.axis([n[0],n[len(n)-1],0,2])
-plt.ylabel('')
-plt.xlabel('n')
-plt.show()
+myStemplot(n,x,'n','x[n]','Reiknirit1')
 
 print(x[0])
 print(y[0])
 
-z = np.convolve(x[0],y[0])
+z = np.convolve(x,y)
 print(z)
 print(len(z))
 print(z[20])
@@ -49,9 +49,4 @@ n = np.linspace(-15,13,29)
 print(n)
 print(len(n))
 
-markerline, stemlines1, basline = plt.stem(n,z,linefmt = 'b')
-plt.setp(stemlines1, 'linewidth','2.0')
-plt.axis([n[0],n[len(n)-1],0,2])
-plt.ylabel('')
-plt.xlabel('n')
-plt.show()
+myStemplot(n,z,'n','z[n]','Reiknirit1')
