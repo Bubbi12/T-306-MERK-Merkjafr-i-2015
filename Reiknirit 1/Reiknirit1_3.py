91b0126509f0a97f2a99ef78465008d387b2b773
Arnar <addi1204@gmail.com>
2015-02-01 19:52:34 +0000

    for part3

--- Reiknirit 1/Reiknirit1_3.py
+++ Reiknirit 1/Reiknirit1_3.py
@@ -24,16 +24,34 @@ def sWF(T1,T,N):
         else:
             ak[stakes] = np.sin(k*T1*2*np.pi/T)/(k*np.pi)
         stakes += 1
-    print(ak)
+    #print(ak)
     return ak, klist
 
 N = 10
 ak,n = sWF(1,4,N)
-myStemplot(n,ak,'[n]','ak[n]','Reiknirit 1 part 3')
+#myStemplot(n,ak,'[n]','ak[n]','Reiknirit 1 part 3')
 ak,n = sWF(1,8,N)
-myStemplot(n,ak,'[n]','ak[n]','Reiknirit 1 part 3')
+#myStemplot(n,ak,'[n]','ak[n]','Reiknirit 1 part 3')
 ak,n = sWF(1,16,N)
-myStemplot(n,ak,'[n]','ak[n]','Reiknirit 1 part 3')
+#myStemplot(n,ak,'[n]','ak[n]','Reiknirit 1 part 3')
 ak,n = sWF(2,16,N)
-myStemplot(n,ak,'[n]','ak[n]','Reiknirit 1 part 3')
+#myStemplot(n,ak,'[n]','ak[n]','Reiknirit 1 part 3')
 
+
+
+
+########################
+# 2
+########################
+T1 = 1
+T = 4
+ak,n = sWF(T1,T,N)
+tt = np.arange(-T/2,T/2,0.001)
+xn = []
+for t in tt:
+    xnt = 0
+    for k in np.arange(-N,N+1,1):
+        xnt += ak[k+N]*np.exp((1j*k*2*np.pi*t)/T)
+    xn.append(xnt)
+print(xn)
+myStemplot(tt,xn,'[n]','ak[n]','Reiknirit 1 part 3')
