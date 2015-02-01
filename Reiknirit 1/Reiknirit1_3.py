3b6cd9b617e788e611287bd5cb333103ce13e3c8
Arnar <addi1204@gmail.com>
2015-02-01 19:07:45 +0000

    for part3

--- Reiknirit 1/Reiknirit1_3.py
+++ Reiknirit 1/Reiknirit1_3.py
@@ -3,18 +3,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def myStemplot(x,y,xl,yl,name):
+    plt.figure(name)
+    markerline, stemlines, baseline = plt.stem(x,y,linefmt = '-')
+    plt.setp(stemlines,'color','b', 'linewidth','2.0')
+    plt.axis([x[0],x[len(x)-1],-1,2])
+    plt.ylabel(yl)
+    plt.xlabel(xl)
+    plt.grid(True)
+    plt.show()
+
 def sWF(T1,T,N):
     a0 = (2*T1)/T
-    k = 2*N+1
-    ak = np.zeros(k)
+    ak = np.zeros(2*N+1)
     stakes = 0
-    for i in np.arange(-N,N+1,1):
-        if i == 0:
+    klist = np.arange(-N,N+1,1)
+    for k in klist:
+        if k == 0:
             ak[stakes] = a0
         else:
-            ak[stakes] = np.sin(i*T1*2*np.pi/T)/(i*np.pi)
+            ak[stakes] = np.sin(k*T1*2*np.pi/T)/(k*np.pi)
         stakes += 1
     print(ak)
-    return ak
+    return ak, klist
 
-sWF(1,4,10)
+N = 10
+ak,n = sWF(1,4,N)
+myStemplot(n,ak,'[n]','ak[n]','Reiknirit 1 part 3')
+ak,n = sWF(1,8,N)
+myStemplot(n,ak,'[n]','ak[n]','Reiknirit 1 part 3')
+ak,n = sWF(1,16,N)
+myStemplot(n,ak,'[n]','ak[n]','Reiknirit 1 part 3')
+ak,n = sWF(2,16,N)
+myStemplot(n,ak,'[n]','ak[n]','Reiknirit 1 part 3')
+
