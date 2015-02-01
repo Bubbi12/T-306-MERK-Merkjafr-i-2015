59b96e0f26c0621f8e23ab1ba0d5b2e9bfd19961
Arnar <addi1204@gmail.com>
2015-02-01 18:29:25 +0000

    added part 2

---
+++ Reiknirit 1/Reiknirit1_2.py
@@ -0,0 +1,60 @@
+# Reiknirit 1 fyrir Merkjafræði 2015
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def myDFT(x):
+    N = len(x)
+    F = []
+    for k in range(N):
+        Fk = 0
+        for n in range(N):
+            Fk += x[n]*np.exp(-1j*2*np.pi*k*n/N)
+        F.append(Fk)
+    return F
+
+def boxsignal(VecStart,VecValueStart, VecValueEnd, VecEnd):
+    length = VecEnd-VecStart+1
+    h = np.zeros(length)
+    n = np.arange(VecStart,VecEnd+1,1,dtype=int)
+    for i in n:
+        if i >= VecValueStart and i <= VecValueEnd:
+            h[i-VecStart] = 1
+    return h,n
+
+def myStemplot(x,y,xl,yl,name):
+    plt.figure(name)
+    markerline, stemlines, baseline = plt.stem(x,y,linefmt = '-')
+    plt.setp(stemlines,'color','b', 'linewidth','2.0')
+    plt.axis([x[0],x[len(x)-1],-10,10])
+    plt.ylabel(yl)
+    plt.xlabel(xl)
+    plt.grid(True)
+    plt.show()
+
+xmin = -8
+xmax = 6
+length = xmax-xmin+1
+n = np.linspace(xmin,xmax,length)
+
+
+
+def compare(h):
+    print('\nMy h of length %i:\n'%len(h),h)
+    dft = myDFT(h)
+    fft = np.fft.fft(h)
+    print('\nMy dft of length %i:\n'%len(dft),dft)
+    print('\nMy real dft of length %i:\n'%len(dft),np.real(dft))
+    print('\nMy fft of length %i:\n'%len(fft),np.real(fft))
+
+    n3 = np.linspace(np.min(n),np.max(n),len(h))
+    myStemplot(n3,fft,'n','h[n]','Reiknirit1')
+
+h,n = boxsignal(-8,1,4,6)
+myStemplot(n,h,'n','h[n]','Reiknirit1')
+compare(h)
+
+
+h = np.sin(np.pi/2*n)
+myStemplot(n,h,'n','h[n]','Reiknirit1')
+compare(h)
