c9f7522ca2da4c7fed6a0918e9132cca37901332
Arnar <addi1204@gmail.com>
2015-02-01 14:14:55 +0000

    offset

--- Reiknirit 1/Reiknirit1.py
+++ Reiknirit 1/Reiknirit1.py
@@ -8,7 +8,7 @@ def myStemplot(x,y,xl,yl,name):
     markerline, stemlines, baseline = plt.stem(x,y,linefmt = '-.')
     plt.setp(stemlines, 'linewidth','2.0')
     plt.setp(baseline, 'color','r', 'linewidth', 2)
-    plt.axis([x[0],x[len(x)-1],0,2])
+    plt.axis([x[0],x[len(x)-1],-1,10])
     plt.ylabel(yl)
     plt.xlabel(xl)
     plt.grid(True)
@@ -21,6 +21,7 @@ length = xmax-xmin+1
 
 y = np.zeros(length)
 n = np.linspace(xmin,xmax,length)
+n2 = np.linspace(2*xmin,2*xmax,2*length-1)
 
 y[3-xmin] = 1
 
@@ -28,7 +29,7 @@ myStemplot(n,y,'n','y[n]','Reiknirit1')
 
 
 
-x = np.zeros(15)
+x = np.zeros(length)
 x[1-xmin] = 1
 x[2-xmin] = 1
 x[3-xmin] = 1
@@ -39,8 +40,18 @@ myStemplot(n,x,'n','x[n]','Reiknirit1')
 z = np.convolve(x,y)
 print(n)
 print(len(n))
-n = np.linspace(2*xmin,2*xmax,2*length-1)
-print(n)
-print(len(n))
 
-myStemplot(n,z,'n','z[n]','Reiknirit1')
+print(n2)
+print(len(n2))
+
+myStemplot(n2,z,'n','z[n]','Reiknirit1')
+
+q = np.zeros(length)
+q[1-xmin] = 0
+q[2-xmin] = 1
+q[3-xmin] = 2
+q[4-xmin] = 1
+q[5-xmin] = 0
+
+r = np.convolve(x,q)
+myStemplot(n2,r,'n','z[n]','Reiknirit1')
