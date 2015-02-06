4e7f43295e4c0a94bce2c1786779d8592206f462
Bubbi12 <hjorleifur02@gmail.com>
2015-02-06 00:59:08 +0000

    The Master Of Domain

--- Reiknirit 1/Reiknirit1_2.py
+++ Reiknirit 1/Reiknirit1_2.py
@@ -1,6 +1,7 @@
 # Reiknirit 1 fyrir Merkjafræði 2015
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 def myDFT(x):
@@ -9,44 +10,28 @@ def myDFT(x):
     for k in range(N):
         Fk = 0
         for n,val in enumerate(x):
-            Fk += x[n] * np.exp(-1j*2*np.pi*k*n/N)
-        F.append(Fk)
-    return F
-def myDFT2(x):
-    N = len(x)
-    F = []
-    for k in range(N):
-        Fk = 0
-        for n in range(N):
-            Fk += x[n]*np.exp(-1j*2*np.pi*k*n/N)
+
+            Fk += x[n] * np.exp(1j*2*np.pi*k*n/N) # It dosnt matter if we have -J og +J 
         F.append(Fk)
     return F
 
-def myDFT_THOR(x):
-    N = len(x)
-    ak = [0] * N
-    for k in range(N):
-        for n in range(N):
-            ak[k] += x[n] * np.exp(-1j*k*((2*np.pi)/N)*n)
-    return ak
 
 def boxsignal(VecStart,VecValueStart, VecValueEnd, VecEnd):
     length = VecEnd-VecStart+1
     h = np.zeros(length)
     n = np.arange(VecStart,VecEnd+1,1,dtype=int)
-    print('my n',n)
     for i in n:
         if i >= VecValueStart and i <= VecValueEnd:
             h[i-VecStart] = 1
-    print(n)
     return h,n
 
-def myStemplot(x,y,xl,yl,name):
+def myStemplot(x,y,xl,yl,name, title = ''):
     plt.figure(name)
     markerline, stemlines, baseline = plt.stem(x,y,linefmt = '-')
     plt.setp(stemlines,'color','b', 'linewidth','2.0')
-    plt.axis([x[0]-10,x[len(x)-1]+10,-30,30])
+    plt.axis([-20,20,-5,5])
     plt.ylabel(yl)
+    plt.title(title)
     plt.xlabel(xl)
     plt.grid(True)
     plt.show()
@@ -54,35 +39,56 @@ def myStemplot(x,y,xl,yl,name):
 
 
 
-def compare(h,n):
-    print('\nMy h of length %i:\n'%len(h),h)
+def compare(h,n, title):
     dft = myDFT(h)
-    dftTHOR = myDFT_THOR(h)
     fft = np.fft.fft(h)
-    print('\nMy dft of length %i:\n'%len(dft),dft)
-    #print('\nMy THOR of length %i:\n'%len(dftTHOR),dftTHOR)
     
-    #print('\nMy real dft of length %i:\n'%len(dft),np.real(dft))
-    print('\nMy fft of length %i:\n'%len(fft),fft)
+    df_Fourier = pd.DataFrame({'Values from myDFT function':dft, 'Values from fft function':fft})
+
+    print(df_Fourier)
+
     dft = fft
 
     dft_Real = []
     dft_Im = []
     dft_Abs = []
     dft_Ang = []
+    fft_Real = []
+    fft_Im = []
+    fft_Abs = []
+    fft_Ang = []
     for i in n:
-        #print('i:',i,'n:',n[i])
         dft_Real.append(np.real(dft[i]))
         dft_Im.append(np.imag(dft[i]))
         dft_Abs.append(np.absolute(dft[i]))
         dft_Ang.append(np.angle(dft[i]))
+        fft_Real.append(np.real(dft[i]))
+        fft_Im.append(np.imag(dft[i]))
+        fft_Abs.append(np.absolute(dft[i]))
+        fft_Ang.append(np.angle(dft[i]))
 
-    plt.figure()
+    df_Fourier_Real = pd.DataFrame({'Real Values from myDFT':dft_Real, 'Real Value from fft':fft_Real})
+
+    print(df_Fourier_Real)
+
+    df_Fourier_Img = pd.DataFrame({'Imag Values from myDFT':dft_Im, 'Imag Value from fft':fft_Im})
+
+    print(df_Fourier_Img)
+
+    df_Fourier_Abs = pd.DataFrame({'Abs Values from myDFT':dft_Abs, 'Abs Value from fft':fft_Abs})
+
+    print(df_Fourier_Abs)
+
+    df_Fourier_Ang = pd.DataFrame({'Angular Values from myDFT':dft_Ang, 'Angular Value from fft':fft_Ang})
+
+    print(df_Fourier_Ang)
+
+    plt.figure('Reiknirit 1')
     plt.subplot(3, 1, 1)
     plt.stem(n, h)
     plt.ylabel('x[n]')
-    plt.title('Reiknirit 3')
-
+    plt.title(title)
+    
     plt.subplot(3, 2, 3)
     plt.stem(n, dft_Real)
     plt.ylabel('RealValue')
@@ -107,10 +113,10 @@ N1 = N/2
 
 
 h,n = boxsignal(-N,-N/2,N/2,N)
-myStemplot(n,h,'n','h[n]','Reiknirit1')
-compare(h,n)
+myStemplot(n,h,'n','h[n]','Reiknirit1','The Box')
+compare(h,n, 'Discrete Fourier Transformation of Box Signal')
 
 n = np.arange(-2,2+1,1,dtype=int)
 h = np.sin(np.pi/2*n)
-myStemplot(n,h,'n','h[n]','Reiknirit1')
-compare(h,n)
+myStemplot(n,h,'n','h[n]','Reiknirit1', 'The Sinusoidal Wave')
+compare(h,n, 'Discrete Fourier Transformation of Sinusodial Wave')
