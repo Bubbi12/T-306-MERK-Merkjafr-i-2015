4d77cdb079c0b6efc2d71c1923939aa6fecbd437
Snorri <snorris13@ru.is>
2015-03-11 14:36:46 +0000

    3fsf

--- Reiknirit 2/hluti1.m
+++ Reiknirit 2/hluti1.m
@@ -10,49 +10,22 @@ Ts = 1/fs; %Þetta er óþarfi.. bara til að sýna Ts
 n = -400:1:400;
 t = n/fs;% = n*Ts
 
-xn1 =  A(1)*sin(2*pi*f(1)*t + fasi(1));  %xt_1
-xn2 =  A(2)*sin(2*pi*f(2)*t + fasi(2));  %xt_2
-xn3 =  A(3)*sin(2*pi*f(3)*t + fasi(3));  %xt_3
+%% --------- Liður 1 - 4 ---------
 
-% --------- Liður 1. ---------
+xn1 =  A(1)*sin(2*pi*f(1)*t + fasi(1));  
+xn2 =  A(2)*sin(2*pi*f(2)*t + fasi(2));  
+xn3 =  A(3)*sin(2*pi*f(3)*t + fasi(3));  
 
-subplot(3,1,1)
-stem(t,xn1)
-
-subplot(3,1,2)
-stem(t,xn2)
-
-subplot(3,1,3)
-stem(t,xn3)
-
-%% --------- Liður 2. ---------
-
-p = 0.4;
-soundsc(xn1,fs)
-pause(p)
-soundsc(xn2,fs)
-pause(p)
-soundsc(xn3,fs)
-pause(p)
-
-%% --------- Liður 3. ---------
-
-xn = xn1+xn2+xn3;
-soundsc(xn,fs)
-figure
-stem(t,xn)
-
-%% --------- Liður 4. ---------
+task1to4(fs,n,xn1,xn2,xn3);
 
 
-subplot(3,1,1)
-stem(t,mag(fft(xn1)))
+ 
+%% --------- Liður 5. ---------
 
-subplot(3,1,2)
-stem(t,mag(fft(xn2)))
+f = [2500 3500 5500]; % [f1 f2 f3] tíðni, Hz
 
-subplot(3,1,3)
-stem(t,mag(fft(xn3)))
+xn1 =  A(1)*sin(2*pi*f(1)*t + fasi(1)); 
+xn2 =  A(2)*sin(2*pi*f(2)*t + fasi(2));  
+xn3 =  A(3)*sin(2*pi*f(3)*t + fasi(3));  
 
-
-
+task1to4(fs,n,xn1,xn2,xn3);
